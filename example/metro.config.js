0ac36e98906110bef24938d9a50f2b79c175eb21
Zoe Roux <zoe.roux@zoriya.dev>
2026-05-28 12:38:59 +0200

    Setup react native web

--- example/metro.config.js
+++ example/metro.config.js
@@ -2,6 +2,8 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const path = require("node:path");
 const root = path.resolve(__dirname, "..");
 
+const useWeb = process.env.USE_WEB === "true" || process.env.PLATFORM === "web";
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -9,7 +11,22 @@ const root = path.resolve(__dirname, "..");
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-	watchFolders: [root],
+  watchFolders: [root],
+  ...(useWeb && {
+    resolver: {
+      extraNodeModules: {
+        "react-native": path.resolve(__dirname, "node_modules", "react-native-web"),
+      },
+    },
+  }),
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
---
+++ example/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const appDirectory = path.resolve(__dirname);
+const rootDirectory = path.resolve(__dirname, "..");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: path.resolve(__dirname, "index.web.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.web.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".web.tsx", ".web.ts", ".tsx", ".ts", ".web.js", ".js"],
+    alias: {
+      "react-native$": "react-native-web",
+      "react-native-omni": path.resolve(rootDirectory, "src"),
+      react: path.resolve(appDirectory, "node_modules", "react"),
+      "react-dom": path.resolve(appDirectory, "node_modules", "react-dom"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules\/(?!(react-native|@react-native|react-native-web|react-native-omni)\/).*/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+            plugins: [
+              ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
+              [
+                "module-resolver",
+                {
+                  extensions: [".js", ".ts", ".json", ".jsx", ".tsx"],
+                  alias: {
+                    "react-native-omni": path.resolve(rootDirectory, "src"),
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
